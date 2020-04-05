2aabf45d0ddd41bd35ab999fd77bacc4f0a2f697
Dreamer <dreamerandprogrammer@gmail.com>
2020-04-05 14:16:26 +0300

    Initial commit

---
+++ FacebookPanoramaCalculator/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace FacebookPanoramaCalculator
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.tb1 = new System.Windows.Forms.TextBox();
+			this.tb2 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.tb3 = new System.Windows.Forms.TextBox();
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.button2 = new System.Windows.Forms.Button();
+			this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(16, 90);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(174, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Calculate";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// tb1
+			// 
+			this.tb1.Location = new System.Drawing.Point(57, 12);
+			this.tb1.Name = "tb1";
+			this.tb1.Size = new System.Drawing.Size(133, 20);
+			this.tb1.TabIndex = 1;
+			this.tb1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tb1_KeyPress);
+			// 
+			// tb2
+			// 
+			this.tb2.Location = new System.Drawing.Point(57, 38);
+			this.tb2.Name = "tb2";
+			this.tb2.Size = new System.Drawing.Size(133, 20);
+			this.tb2.TabIndex = 2;
+			this.tb2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tb2_KeyPress);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(13, 15);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(38, 13);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "Height";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(13, 41);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(35, 13);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "Width";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(13, 67);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(20, 13);
+			this.label3.TabIndex = 5;
+			this.label3.Text = "FV";
+			// 
+			// tb3
+			// 
+			this.tb3.Location = new System.Drawing.Point(57, 64);
+			this.tb3.Name = "tb3";
+			this.tb3.Size = new System.Drawing.Size(133, 20);
+			this.tb3.TabIndex = 6;
+			this.tb3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tb3_KeyPress);
+			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.Location = new System.Drawing.Point(196, 12);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.Size = new System.Drawing.Size(409, 130);
+			this.richTextBox1.TabIndex = 7;
+			this.richTextBox1.Text = "";
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(16, 119);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(174, 23);
+			this.button2.TabIndex = 8;
+			this.button2.Text = "Export (.xmp)";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// saveFileDialog1
+			// 
+			this.saveFileDialog1.Filter = "XMP|*.xmp|All files|*.*";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(617, 152);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.richTextBox1);
+			this.Controls.Add(this.tb3);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.tb2);
+			this.Controls.Add(this.tb1);
+			this.Controls.Add(this.button1);
+			this.Name = "Form1";
+			this.Text = "Facebook 360 calculator";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox tb1;
+		private System.Windows.Forms.TextBox tb2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox tb3;
+		private System.Windows.Forms.RichTextBox richTextBox1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+	}
+}
+
---
+++ FacebookPanoramaCalculator/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;            // File
+using System.Text.RegularExpressions; // Regex
+
+namespace FacebookPanoramaCalculator
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			int height = Int32.Parse(tb1.Text);
+			int width = Int32.Parse(tb1.Text);
+
+			int fv = Int32.Parse(tb3.Text);
+
+			int width_full = (360 * width) / fv;
+			int height_full = width_full / 2;
+
+			int cp_left = (width_full - width) / 2;
+			int cp_top = (height_full - height) / 2;
+
+			string pattern = $"<GPano:CroppedAreaImageHeightPixels>{height}</GPano:CroppedAreaImageHeightPixels>\n< GPano:CroppedAreaImageWidthPixels >{width}</ GPano:CroppedAreaImageWidthPixels >\n< GPano:FullPanoHeightPixels >{height_full}</ GPano:FullPanoHeightPixels >\n< GPano:FullPanoWidthPixels >{width_full}</ GPano:FullPanoWidthPixels >\n< GPano:CroppedAreaLeftPixels >{cp_left}</ GPano:CroppedAreaLeftPixels >\n< GPano:CroppedAreaTopPixels >{cp_top}</ GPano:CroppedAreaTopPixels >";
+
+			richTextBox1.Text = pattern;
+		}
+
+		private void tb1_KeyPress(object sender, KeyPressEventArgs e)
+		{
+			// ввод в texBox только цифр и кнопки Backspace
+			char ch = e.KeyChar;
+			if (!Char.IsDigit(ch) && ch != 8)
+			{
+				e.Handled = true;
+			}
+		}
+
+		private void tb2_KeyPress(object sender, KeyPressEventArgs e)
+		{
+			// ввод в texBox только цифр и кнопки Backspace
+			char ch = e.KeyChar;
+			if (!Char.IsDigit(ch) && ch != 8)
+			{
+				e.Handled = true;
+			}
+		}
+
+		private void tb3_KeyPress(object sender, KeyPressEventArgs e)
+		{
+			// ввод в texBox только цифр и кнопки Backspace
+			char ch = e.KeyChar;
+			if (!Char.IsDigit(ch) && ch != 8)
+			{
+				e.Handled = true;
+			}
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			string[] result_strings = new[] { richTextBox1.Text };
+
+			Regex regEx = new Regex("[A-Za-z0-9_-]*[.]*[A-Za-z0-9]{3,4}");
+
+			if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+			{
+				try
+				{
+					string path = saveFileDialog1.FileName;
+
+					if (!regEx.Match(path).Success)
+					{
+						path += ".xmp";
+					}
+
+					File.WriteAllLines(path, result_strings);
+				}
+				catch
+				{
+					MessageBox.Show("Error while saving file", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				}
+			}
+		}
+	}
+}
